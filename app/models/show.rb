866e4de160c45059b335857f60f8276b3f9a36f1
ben <benjaminskingsley@gmail.com>
2021-02-22 16:25:14 +0000

    Added Cloudinary to project

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,6 +1,7 @@
 class Show < ApplicationRecord
   has_many :bookings
   belongs_to :user
+  has_one_attached :photo
 
   SHOWTIMES = (9..22).to_a
   validates :capacity, presence: true, numericality: { less_than_or_equal_to: 50, only_integer: true }
