c22616507e7b45fced9c1523c79b582abfae352c
Mark Price <markempower94@gmail.com>
2021-02-23 11:48:06 +0000

    add login and logout buttons to navbar

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :shows, only: [:show, :index, :new, :create]
+
+  resources :users, only: [:show, :edit, :update]
 end
