b2da052789a3512d15f272e2f0c7aac4739eeb11
Matt Smith <jetnova@pm.me>
2021-02-23 15:16:17 +0000

    booking form done, and redirects to user's booking page

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -22,11 +22,12 @@ class BookingsController < ApplicationController
 
   # Create action to create a booking (from POST request)
   def create
-    attrs = { no_of_people: booking_params, show_id: params[:show_id], user_id: current_user.id }
-    @booking = Booking.new(attrs)
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.show = Show.find(params[:show_id])
     if @booking.save
       # redirects to the user's booking page for that booking if successful
-      redirect_to user_booking_path(@user, @booking), notice: 'Booking Successful!'
+      redirect_to user_booking_path(@booking.user, @booking.id), notice: 'Booking Successful!'
     else
       render :new
     end
