da6981f4d442b19ac4e300ed6a9640488887204d
Matt Smith <jetnova@pm.me>
2021-02-25 12:21:02 +0000

    remaining seats helper implemented, and called in appropriate places in view templates

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,6 +5,7 @@ module ApplicationHelper
 
   end
 
+  # time formatter helper, where a string is returned depending on the hour integer arguement
   def time_formatter(hour)
     if hour >= 12 && hour <= 24
       hour == 12 ? twelve_hr = hour : twelve_hr = (hour - 12)
@@ -12,5 +13,15 @@ module ApplicationHelper
     else
       return "#{hour}:00 am"
     end
+  end
+
+  # remaining seats helper, where a string is returned indicating availability for a show
+  def remaining_seats(show)
+    booked = 0
+    Booking.where(show_id: show.id).each do |booking|
+      booked += booking.no_of_people
+    end
+    remaining = show.capacity - booked
+    return remaining > 0 ? "Available Seats - #{remaining}" : "SOLD OUT!"
   end
 end
