bcdedb9ec6d1615215db071c574174d28972004f
ben <benjaminskingsley@gmail.com>
2021-02-26 15:44:12 +0000

    Commented counter

--- app/javascript/controllers/counter_controller.js
+++ app/javascript/controllers/counter_controller.js
@@ -1,18 +1,18 @@
-import { Controller } from "stimulus";
+// import { Controller } from "stimulus";
 
-export default class extends Controller {
-    static targets = [ 'count' ];
+// export default class extends Controller {
+//     static targets = [ 'count' ];
 
-    connect() {
-        setInterval(this.refresh, 3000);
-    }
+//     connect() {
+//         setInterval(this.refresh, 3000);
+//     }
 
-    refresh = () => {
-        fetch('/shows', { headers: { accept: "application/json" } })
-            .then(response => response.json())
-            .then((data) => {
-                this.countTarget.innerText = data.shows.length;
-                console.log(data);
-            });
-    }
-}
+//     refresh = () => {
+//         fetch('/shows', { headers: { accept: "application/json" } })
+//             .then(response => response.json())
+//             .then((data) => {
+//                 this.countTarget.innerText = data.shows.length;
+//                 console.log(data);
+//             });
+//     }
+// }
